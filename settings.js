132b985b99133bb901ec57f7fff832ff5baf75a2
M4TEO FX <jazinmelenaiximenha@gmail.com>
2026-08-06 14:39:24 -0600

    Creo que lo puse bien 💀XDDD

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-global.owner = ['12602764655', '5216242255295', '5216241181534', '584242773183']
+global.owner = ['12602764655', '5216242255295', '5217531631528', '5216241181534', '584242773183']
 global.sessionName = 'Sessions/Owner'
 global.version = '^3.0.9'
 global.pairing_code = true
