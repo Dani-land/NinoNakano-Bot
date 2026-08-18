cad33f05709b90a182119a0034733edb65c9dce1
Danielrxz <danielrodigome@gmail.com>
2026-08-18 08:22:44 -0700

    Actualizar rw.js

--- commands/gacha/rw.js
+++ commands/gacha/rw.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch';
 const obtenerImagen = async (keyword) => {
   const urls = [
     `https://api.stellarwa.xyz/home#nsfw/nsfw-danbooru?keyword=${encodeURIComponent(keyword)}`,
-    `https://api.stellarwa.xyz/nsfw/gelbooru?keyword=${encodeURIComponent(keyword)}`
+    `https://api.stellarwa.xyz/home#nsfw/nsfw-gelbooru?keyword=${encodeURIComponent(keyword)}`
   ];
 
   for (const url of urls) {
